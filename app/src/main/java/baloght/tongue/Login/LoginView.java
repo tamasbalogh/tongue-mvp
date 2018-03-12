b75a4e0259d1dffdfc61725ba325bd0935bc1e7e
tamasbalogh <baloght@gain.uni-sopron.hu>
2018-03-12 14:44:31 +0100

    Login activity

---
+++ app/src/main/java/baloght/tongue/Login/LoginView.java
@@ -0,0 +1,20 @@
+package baloght.tongue.Login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import baloght.tongue.R;
+
+/**
+ * Created by baloght on 2018.03.12..
+ */
+
+public class LoginView extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
